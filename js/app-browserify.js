d429939262866de60211293780fdcbd4151a8500
Michael Cheng <michael.zhang.cheng@gmail.com>
2015-06-17 13:42:25 -0500

    backbone

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -2,8 +2,11 @@
 require("es5-shim")
 require("babel/register")
 var Promise = require('es6-promise').Promise
+var $ = require('jquery')
+var _ = require('underscore')
 var Backbone = require('backbone')
 var React = require('react')
+
 //essentials
 var apikey = `c1e08233049fb45bea793e4c0e7e4b27`
 var secret = `ff73e90235bd4bd416f27ebd9db2f3fb`
@@ -13,20 +16,26 @@ var qs = (s, d) => (d || document).querySelector(s)
 SC.initialize({
     client_id: apikey,
 })
-// Starts streaming A Song from Soundcloud and plays it.
-// var initial = SC.stream("/tracks/293", function(sound){
-// });
-SC.get('/tracks', { q: 'simon garfunkel live' }, function(tracks) {
-  console.log(tracks);
-})
+
 // Each track's information represented with model
 var SoundcloudModel = Backbone.Model.extend({
-	url: `https://api.soundcloud.com/tracks/${this.id}?client_id=${apikey}`,
+    idAttribute: 'id',
+    url: function () {return `https://api.soundcloud.com/tracks/${this.id}?client_id=${apikey}`}
 })
 
 // A query search of tracks represented by collection
-var SoundcloudCollection = Backbone.Collection.extend({
-	model: SoundcloudModel,
-	url: `https://api.soundcloud.com/tracks?client_id=${apikey}`,
+var SoundSearchCollection = Backbone.Collection.extend({
+    initialize : function() {
+        this.on('sync', console.log(this))
+    }
+    url: function() {return `https://api.soundcloud.com/tracks?q=${this.query}&client_id=${apikey}`}
 })
 
+class ListItem extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        
+    }
+}
